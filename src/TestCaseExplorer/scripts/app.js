963c570ddf4fbd206ea7da5e57d9628151788927
mattias sköld <mattias_skold71@hotmail.com>
2015-09-05 15:27:57 +0000

    Added simple treview and combo - all hard coded...

--- src/TestCaseExplorer/scripts/app.js
+++ src/TestCaseExplorer/scripts/app.js
@@ -1,6 +1,6 @@
 /// <reference path='ref/jquery.d.ts' />
 /// <reference path='ref/VSS.d.ts' />
-define(["require", "exports", "VSS/Controls", "VSS/Controls/Grids", "VSS/Controls/Menus", "TFS/WorkItemTracking/RestClient"], function (require, exports, Controls, Grids, Menus, RestClient) {
+define(["require", "exports", "VSS/Controls", "VSS/Controls/Grids", "VSS/Controls/Menus", "VSS/Controls/TreeView", "VSS/Controls/Common", "TFS/WorkItemTracking/RestClient"], function (require, exports, Controls, Grids, Menus, TreeView, CommonControls, RestClient) {
     var menuItems = [
         { id: "file", text: "New", icon: "icon-add-small" },
         { separator: true },
@@ -35,7 +35,85 @@ define(["require", "exports", "VSS/Controls", "VSS/Controls/Grids", "VSS/Control
         // This data source is rendered into the Grid columns defined above
         source: dataSource
     };
+    // initialise the TreeViewView 
+    InitTreeViewView();
     // Create the grid in a container element
     var grid = Controls.create(Grids.Grid, $("#grid-container"), options);
+    function InitTreeViewView() {
+        var cboSources = ["Area path", "Iteration path", "Priority", "State", "Test plan", "Team"];
+        // Converts the source to TreeNodes
+        function convertToTreeNodes(items) {
+            return $.map(items, function (item) {
+                var node = new TreeView.TreeNode(item.name);
+                node.icon = item.icon;
+                node.expanded = item.expanded;
+                if (item.children && item.children.length > 0) {
+                    node.addRange(convertToTreeNodes(item.children));
+                }
+                return node;
+            });
+        }
+        var cbo = Controls.create(CommonControls.Combo, $("#treeview-Cbo-container"), {
+            source: cboSources
+        });
+        cbo.setText(cboSources[0]);
+        // Generate TreeView options
+        var treeOptions = {
+            width: 400,
+            height: "100%",
+            nodes: convertToTreeNodes(getNodes(cbo.getText()))
+        };
+        var treeview = Controls.create(TreeView.TreeView, $("#treeview-container"), treeOptions);
+        treeview.onItemClick = function (node, nodeElement, e) {
+            $("#treeview-value").text(node.title);
+        };
+        $("#treeview-Cbo-container").change(function () {
+            //treeview.removeNode(treeview.rootNode);
+            treeview.rootNode = convertToTreeNodes(getNodes(cbo.getText()));
+            // treeview.setDataSource(convertToTreeNodes(getNodes(cbo.getText())));
+            //treeOptions.nodes = convertToTreeNodes(getNodes(cbo.getText()));
+            //Controls.create(TreeView.TreeView, $("#treeview-container"), treeOptions);
+        });
+        function getNodes(param) {
+            switch (param) {
+                case "Area path":
+                    return [
+                        {
+                            name: "Project", icon: "icon icon-people", children: [
+                                { name: "Area 1" },
+                                { name: "Area 2" },
+                                { name: "Area 3" },
+                                { name: "Area 4" }],
+                            expanded: true
+                        }
+                    ];
+                    break;
+                case "Iteration path":
+                    return [
+                        {
+                            name: "Project", icon: "icon icon-people", children: [
+                                { name: "Iteration 1" },
+                                { name: "Iteration 2" },
+                                { name: "Iteration 3" },
+                                { name: "Iteration 4" }],
+                            expanded: true
+                        }
+                    ];
+                    break;
+                case "Priority":
+                    return [
+                        {
+                            name: "1"
+                        },
+                        {
+                            name: "2"
+                        },
+                        {
+                            name: "3"
+                        }];
+                    break;
+            }
+        }
+    }
 });
 //# sourceMappingURL=app.js.map
--- src/TestCaseExplorer/scripts/app.ts
+++ src/TestCaseExplorer/scripts/app.ts
@@ -5,6 +5,10 @@ import Controls = require("VSS/Controls");
 import Grids = require("VSS/Controls/Grids");
 import Menus = require("VSS/Controls/Menus");
 
+
+import TreeView = require("VSS/Controls/TreeView");
+import CommonControls = require("VSS/Controls/Common");
+
 var menuItems: Menus.IMenuItemSpec[] = [
     { id: "file", text: "New", icon: "icon-add-small" },
     { separator: true }, 
@@ -47,5 +51,105 @@ var options = {
     source: dataSource
 };
 
+
+// initialise the TreeViewView 
+InitTreeViewView();
+
 // Create the grid in a container element
 var grid = Controls.create<Grids.Grid, Grids.IGridOptions>(Grids.Grid, $("#grid-container"), options);
+
+
+function InitTreeViewView() {
+
+    var cboSources = ["Area path","Iteration path","Priority", "State", "Test plan", "Team"];
+
+        // Converts the source to TreeNodes
+    function convertToTreeNodes(items) {
+        return $.map(items, function (item) {
+            var node = new TreeView.TreeNode(item.name);
+            node.icon = item.icon;
+            node.expanded = item.expanded;
+            if (item.children && item.children.length > 0) {
+                node.addRange(convertToTreeNodes(item.children));
+            }
+            return node;
+        });
+    }
+
+
+    var cbo = Controls.create(CommonControls.Combo, $("#treeview-Cbo-container"), {
+        source: cboSources
+    });
+    cbo.setText(cboSources[0]);
+
+    // Generate TreeView options
+    var treeOptions = {
+        width: 400,
+        height: "100%",
+        nodes: convertToTreeNodes(getNodes(cbo.getText()))
+    };
+    
+
+    var treeview = Controls.create(TreeView.TreeView, $("#treeview-container"), treeOptions);
+    treeview.onItemClick = function (node: TreeView.TreeNode, nodeElement: any, e?: JQueryEventObject): void {
+        $("#treeview-value").text( node.title);
+    };
+
+        
+    $("#treeview-Cbo-container").change(function () {
+        //treeview.removeNode(treeview.rootNode);
+        treeview.rootNode = convertToTreeNodes(getNodes(cbo.getText()));
+
+       // treeview.setDataSource(convertToTreeNodes(getNodes(cbo.getText())));
+
+        //treeOptions.nodes = convertToTreeNodes(getNodes(cbo.getText()));
+
+        //Controls.create(TreeView.TreeView, $("#treeview-container"), treeOptions);
+
+
+    }); 
+
+    function getNodes(param) {
+
+        switch (param) {
+            case "Area path":
+                return [
+                    {
+                        name: "Project", icon: "icon icon-people", children: [
+                            { name: "Area 1" },
+                            { name: "Area 2" },
+                            { name: "Area 3" },
+                            { name: "Area 4" }],
+                        expanded: true
+                    }
+                ];
+                break;
+            case "Iteration path":
+                return [
+                    {
+                        name: "Project", icon: "icon icon-people", children: [
+                            { name: "Iteration 1" },
+                            { name: "Iteration 2" },
+                            { name: "Iteration 3" },
+                            { name: "Iteration 4" }],
+                        expanded: true
+                    }
+                ];
+                break;
+            case "Priority":
+                return [
+                    {
+                        name: "1"
+                    },
+                    {
+                        name: "2"
+                    },
+                    {
+                        name: "3"
+                    }]; 
+                break;
+        }
+
+    }
+
+}
