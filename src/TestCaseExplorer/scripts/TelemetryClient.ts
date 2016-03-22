e9fda8fd2cd21b99bfc3683e578a597295525ed1
Mathias Olausson <mathias@olausson.net>
2016-03-22 19:17:06 +0100

    Adding error handling to AI

--- src/TestCaseExplorer/scripts/TelemetryClient.ts
+++ src/TestCaseExplorer/scripts/TelemetryClient.ts
@@ -11,52 +11,97 @@ class TelemetryClient {
         }
 
         return this.telemetryClient;
-
     }
 
     private appInsightsClient: Microsoft.ApplicationInsights.AppInsights;
 
     private Init() {
-        var snippet: any = {
-            config: {
-                instrumentationKey: "f83f44a3-3005-4754-bf97-f978931244a6",
-            }
-        };
-        var x = VSS.getExtensionContext();
+        try {
+            var snippet: any = {
+                config: {
+                    instrumentationKey: "f83f44a3-3005-4754-bf97-f978931244a6",
+                }
+            };
+            var x = VSS.getExtensionContext();
 
-        var init = new Microsoft.ApplicationInsights.Initialization(snippet);
-        this.appInsightsClient = init.loadAppInsights();
+            var init = new Microsoft.ApplicationInsights.Initialization(snippet);
+            this.appInsightsClient = init.loadAppInsights();
 
-        var webContext = VSS.getWebContext();
-        this.appInsightsClient.setAuthenticatedUserContext(webContext.user.id, webContext.collection.id);
-
+            var webContext = VSS.getWebContext();
+            this.appInsightsClient.setAuthenticatedUserContext(webContext.user.id, webContext.collection.id);
+        }
+        catch (e) {
+            console.log(e);
+        }
     }
 
     public startTrackPageView(name?: string) {
-        this.appInsightsClient.startTrackPage(name);
+        try {
+            if (this.appInsightsClient != null) {
+                this.appInsightsClient.startTrackPage(name);
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
     }
 
     public stopTrackPageView(name?: string) {
-        this.appInsightsClient.stopTrackPage(name);
+        try {
+            if (this.appInsightsClient != null) {
+                this.appInsightsClient.stopTrackPage(name);
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
     }
 
     public trackPageView(name?: string, url?: string, properties?: Object, measurements?: Object, duration?: number) {
-        this.appInsightsClient.trackPageView("TCExplorer." + name, url, properties, measurements, duration);        
+        try {
+            if (this.appInsightsClient != null) {
+                this.appInsightsClient.trackPageView("TCExplorer." + name, url, properties, measurements, duration);        
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
     }
 
     public trackEvent(name: string, properties?: Object, measurements?: Object) {
-        this.appInsightsClient.trackEvent("TCExplorer." + name, properties, measurements);
-        this.appInsightsClient.flush();
+        try {
+            if (this.appInsightsClient != null) {
+                this.appInsightsClient.trackEvent("TCExplorer." + name, properties, measurements);
+                this.appInsightsClient.flush();
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
     }
 
     public trackException(exception: Error, handledAt?: string, properties?: Object, measurements?: Object) {
-        this.appInsightsClient.trackException(exception, handledAt, properties, measurements);
-        this.appInsightsClient.flush();
+        try {
+            if (this.appInsightsClient != null) {
+                this.appInsightsClient.trackException(exception, handledAt, properties, measurements);
+                this.appInsightsClient.flush();
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
     }
 
     public trackMetric(name: string, average: number, sampleCount?: number, min?: number, max?: number, properties?: Object) {
-        this.appInsightsClient.trackMetric("TCExplorer." + name, average, sampleCount, min, max, properties);
-        this.appInsightsClient.flush();
+        try {
+            if (this.appInsightsClient != null) {
+                this.appInsightsClient.trackMetric("TCExplorer." + name, average, sampleCount, min, max, properties);
+                this.appInsightsClient.flush();
+            }
+        }
+        catch (e) {
+            console.log(e);
+        }
     }
 
 }
