671cb5a407efc36c328ab6ee3422a434b15a9bee
mattias sköld <mattias_skold71@hotmail.com>
2016-03-26 21:37:40 +0000

    Fixe capital F for fields

--- src/TestCaseExplorer/scripts/TestCaseView.ts
+++ src/TestCaseExplorer/scripts/TestCaseView.ts
@@ -108,7 +108,7 @@ export class TestCaseView {
                 break;
             case "Test plan":
                 
-                promise = TestCaseDataService.getTestCasesByTestPlan(value.testPlanId, value.suiteId, this._fields.map(f=> { return f.Field }) , showRecursive);
+                promise = TestCaseDataService.getTestCasesByTestPlan(value.testPlanId, value.suiteId, this._fields.map(f=> { return f.field }) , showRecursive);
                 this._fields = this._fields.concat([{ field: "Present.In.Suite", name: "Present in suites", width: 150 }]);
                 title = "Test suite: " + value.name + " (Suite Id: " + value.suiteId + ")";
 
