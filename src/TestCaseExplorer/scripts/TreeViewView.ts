74247ecddfe940072762bd7cab51771313ed78e6
mattias sköld <mattias_skold71@hotmail.com>
2016-09-06 20:51:38 +0000

    Code cleanup

--- src/TestCaseExplorer/scripts/TreeViewView.ts
+++ src/TestCaseExplorer/scripts/TreeViewView.ts
@@ -38,60 +38,26 @@ export class TreeviewView {
     private _currentSource: string;
     private _waitControl: StatusIndicator.WaitControl;
 
-    /*
-    private listenToTheKey(e) {
-        if (e.which === 27 || e.keyCode === 27) {
-            console.log("cancelling drag...");
-            $("li.node").draggable({ 'revert': true }).trigger('mouseup');
-        }
-        else {
-            var mode: string = "";
-            if (e.ctrlKey) {
-                console.log("clone...");
-                mode = "Clone";
-            }
-            else if (e.shiftKey) {
-                console.log("add...");
-                mode = "Add";
-            }
-            else {
-                console.log("move...");
-                mode = "Move";
-            }
-            var text = $(".drag-tile-drag-type").text();
-
-            if (text != "Attach") {
-                $(".drag-tile-drag-type").text(mode);
-            }
-        }
-    }
-    */
+   
 
     public initialize(callback: TreeviewSelectedCallback) {
         TelemetryClient.getClient().trackPageView("TreeView");
         var view = this;
         view._showRecursive = false;
         view._callback = callback;
+
         var cboSources = ["Area path", "Iteration path", "Priority", "State", "Test plan"];
-
-        //window.onkeydown = this.listenToTheKey;
-        //window.onkeyup = this.listenToTheKey;
-
         var cboOptions: CtrlCombos.IComboOptions = {
             mode: "drop",
             allowEdit: false,
             source: cboSources
         };
-
         var cbo = Controls.create(CtrlCombos.Combo, $("#treeview-Cbo-container"), cboOptions);
 
         var treeOptions: TreeView.ITreeOptions = {
             clickSelects: true,
             nodes: null
         };
-
-        this._enableDragDrop(treeOptions);
-
         var treeview = Controls.create(TreeView.TreeView, $("#treeview-container"), treeOptions);
 
         treeview.onItemClick = function (node, nodeElement, e) {
@@ -126,140 +92,10 @@ export class TreeviewView {
 
                 cbo.setText(selectedPivot);
                 view.LoadTreeview(cbo.getText(), treeview);
-
             })
         });
     }
-
-    private _enableDragDrop(options?: any) {
-        var that = this;
-
-        options.draggable = $.extend({
-            scroll: false,
-            scrollables: [".testmanagement-suites-tree"],
-            scope: "test-case-scope", //TestSuitesTree.DropScope,
-            appendTo: document.body,        // append to body to allow for free drag/drop                    
-            distance: 20,                   // start the drag if the mouse moved more than 20px, this will prevent accidential drag/drop
-            //helper: function (event, ui) { return that._draggableHelper(that, event, ui); },
-            helper: function (event, ui) {
-                var $dragTile;
-
-                var title = event.currentTarget.title;
-                var draggedNode = that._treeview.getNodeFromElement(event.currentTarget);
-
-                $dragTile = $("<div />")
-                    .addClass("drag-tile")
-
-                var $dragItemTitle = $("<div />")
-                    .addClass("drag-tile-title")
-                    .text(title);
-
-                var $dragType = $("<span />")
-                    .addClass("drag-tile-drag-type")
-                    .text("Move");
-
-                var $dragHead = $("<div />")
-                    .addClass("drag-tile-head")
-                    .append($dragType)
-                    .append($dragItemTitle)
-
-                $dragTile.append($dragHead);
-
-                $dragTile.data("PLAN_ID", draggedNode.config);
-                $dragTile.data("SUITE_ID", draggedNode.id);
-                $dragTile.data("MODE", "TEST_SUITE");
-
-                return $dragTile;
-            },
-            cursorAt: { left: -20, top: 0 },
-            containment: "", //".testmanagement-suites-tree",
-            //start: function (event, ui) { that._draggableStart(that, event, ui); },
-            //stop: function (event, ui) { that._draggableStop(event, ui); },
-            drag: function (event, ui) { that._draggableDrag(that, event, ui); }
-        }, options.draggable);
-
-        options.droppable = $.extend({
-            //hoverClass: "droppable-hover",
-            scope: "test-case-scope", //TestSuitesTree.DropScope,
-            tolerance: "pointer",
-            //accept: function ($draggable) { return that._droppableAccept(this, $draggable); },
-            /*over: function (e, ui) {
-                if (that._currentSource != "Test plan") {
-                    var $dropTarget = $(e.target);
-                    $dropTarget.addClass("active");
-                }
-            },
-            out: function (e, ui) {
-                if (that._currentSource != "Test plan") {
-                    var $dropTarget = $(e.target);
-                    $dropTarget.removeClass("active");
-                }
-            },*/
-            drop: function (event, ui) { return that._droppableDrop(that, event, ui); },
-            //over: function (event, ui) { that._droppableOver($(this), event, ui); },
-            greedy: true // ensure that the most local/specific elements get to accept the drop first, not the parents                    
-        }, options.droppable);
-    }
-
-    private _draggableHelper(that: TreeviewView, event, ui) {
-        var $dragTile;
-
-        var title = event.currentTarget.title;
-        var draggedNode = that._treeview.getNodeFromElement(event.currentTarget);
-
-        $dragTile = $("<div />")
-            .addClass("drag-tile")
-
-        var $dragItemTitle = $("<div />")
-            .addClass("drag-tile-title")
-            .text(title);
-
-
-        var $dragType = $("<span />")
-            .addClass("drag-tile-drag-type")
-            .text("Move");
-
-        var $dragHead = $("<div />")
-            .addClass("drag-tile-head")
-            .append($dragType)
-            .append($dragItemTitle)
-
-        $dragTile.append($dragHead);
-
-        $dragTile.data("PLAN_ID", draggedNode.config);
-        $dragTile.data("SUITE_ID", draggedNode.id);
-        $dragTile.data("MODE", "TEST_SUITE");
-
-        return $dragTile;
-    }
-    private _draggableStart(that: TreeviewView, event, ui) {
-    }
-    private _draggableStop(event, ui) {
-    }
-    private _draggableDrag(that: TreeviewView, event, ui) {
-    }
-    private _droppableAccept(that: TreeviewView, event) {   // argument?
-        return that._currentSource == "Test plan" ? false : true;
-    }
-    private _droppableDrop(that: TreeviewView, event, ui) {
-        var n: TreeView.TreeNode = that._treeview.getNodeFromElement(event.target);
-
-        var action = ui.helper.data("MODE");  // TODO: rename to action
-        var mode = that.getCurrentDragMode(event);
-
-        switch (action) {
-            case "TEST_CASE":
-                that.processDropTestCase(ui, n, that._currentSource, mode);
-                break;
-            default:    // TODO: verify this should not happen
-                console.log("treeview::drop - undefined action");
-                break;
-        }
-    }
-    private _droppableOver(that: TreeviewView, event, ui) {
-
-    }
-
+    
     private openTestSuite() {
         var url = VSS.getWebContext().collection.uri;
         var project = VSS.getWebContext().project.name;
@@ -412,123 +248,41 @@ export class TreeviewView {
                 treeview._setNodeExpansion(n, elem, true);
             });
 
-            if (view._currentSource == "Test plan") {
+            if (view._currentSource == "Test plan"){ 
                 $("li.node").draggable({
+                    distance: 10,
+                    cursorAt: { top: -5, left: -5 },
+                    refreshPositions: true,
+                    scroll: true,
                     scope: "test-case-scope",
                     //revert: "invalid",
                     appendTo: document.body,
                     helper: function (event, ui) {
-                        var $dragTile;
-
                         var title = event.currentTarget.title;
                         var draggedNode = view._treeview.getNodeFromElement(event.currentTarget);
 
-                        $dragTile = $("<div />")
-                            .addClass("drag-tile")
-
-                        var $dropAllowed = $("<div />").addClass("drop-allowed");
-                        var $dropNOTAllowed = $("<div style='display:none'/>").addClass("drop-not-allowed");
-                        var $dropNOTAllowedIcon = $("<img src='img/cancel.png'/>")
-                        var $dropNotAllowedMsg = $("<div>").addClass("drop-not-allowed-message");
-
-                        //var $dragItemTitle = $("#tfs_tnli" + draggedNode.id).clone();
-                        //$dragItemTitle.find(".tree-children").remove();
-                        //$dragItemTitle.find(".tree-children").remove();
-                        //$dragItemTitle = $dragItemTitle.find(".node-content").removeAttr("style");
                         var $dragItemTitle = $("<div />").addClass("node-content");
-                        var $dragItemIcon = $("<span class='icon tree-node-img' />").addClass(draggedNode.icon)
+                        var $dragItemIcon = $("<span class='icon tree-node-img' />").addClass(draggedNode.icon);
                         $dragItemTitle.append($dragItemIcon);
                         $dragItemTitle.append($("<span />").text(draggedNode.text));
-
                         $dragItemTitle.css("width", event.currentTarget.clientWidth);
 
-                        var $dragType = $("<span />")
-                            .addClass("drag-tile-drag-type")
-                            .text("Move");
-
-                        var $dragHead = $("<div />")
-                            .addClass("drag-tile-head")
-                            .append($dragType)
-                            .append($dragItemTitle)
-
-                        $dropAllowed.append($dragHead);
-                        $dropNOTAllowed.append($dropNOTAllowedIcon);
-                        $dropNOTAllowed.append($dropNotAllowedMsg);
-
-                        $dragTile.append($dropAllowed);
-                        $dragTile.append($dropNOTAllowed);
-
+                        var $dragTile = Common.createDragTile("MOVE", $dragItemTitle);
                         $dragTile.data("PLAN_ID", draggedNode.config);
                         $dragTile.data("SUITE_ID", draggedNode.id);
                         $dragTile.data("MODE", "TEST_SUITE");
 
                         return $dragTile;
-                    },
-                    distance: 10,
-                    cursorAt: { top: -5, left: -5 },
-                    refreshPositions: true,
-                    scroll: true //,
-                    //stop: function () {
-                    //    // Set all draggable parts back to revert: false
-                    //    // This fixes elements after drag was cancelled with ESC key
-                    //    $("li.node").draggable("option", { revert: false });
-                    //}
+                    }
                 });
             }
-
-            /*
-            $("li.node").droppable({
-                scope: "test-case-scope",
-                greedy: true,
-                tolerance: "pointer",
-                accept: function (d) {
-                    var t = this;
-                    var text = $(this).text;
-                    return true;
-                },
-                over: function (e, ui) {
-                    var target: any = e.target;
-                    console.log("over " + target.title);
-
-                    var n: TreeView.TreeNode = treeview.getNodeFromElement(e.target);
-                    //if (n.type == "Static suite") {
-                    //    $(e.target).addClass("drag-hover");
-                    //}
-                    //else {
-                    //    $(e.target).addClass("drag-hover-invalid");
-                    //}
-                },
-                out: function (e, ui) {
-                    var target: any = e.target;
-                    console.log("out " + target.title);
-
-                    //$(e.target).removeClass("drag-hover drag-hover-invalid");
-                },
-                drop: function (event: any, ui) {
-
-                    var n: TreeView.TreeNode = treeview.getNodeFromElement(event.target);
-
-                    var action = ui.helper.data("MODE");  // TODO: rename to action
-                    var mode = view.getCurrentDragMode(event);
-
-                    switch (action) {
-                        case "TEST_CASE":
-                            view.processDropTestCase(ui, n, view._currentSource, mode);
-                            break;
-                        default:    // TODO: verify this should not happen
-                            console.log("treeview::drop - undefined action");
-                            break;
-                    }
-                }
-            });
-            */
 
             deferred.resolve(data);
         });
 
         return deferred.promise();
     }
-
+    
     // TODO: refactor to enum
     private getCurrentDragMode(event): string {
         var mode = "MOVE";
@@ -591,7 +345,6 @@ export class TreeviewView {
 
             alert("Not supported in this version");
         }
-
     }
 }
 
